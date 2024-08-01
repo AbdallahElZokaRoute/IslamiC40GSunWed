4a07dfaf9f9ea4733b27ab4c7a7c9c10c344b44f
ahmed.gamal <Ahmed_elhabyan@rayacorp.com>
2024-08-01 21:22:38 +0300

    Add Tassbeh Fragment and colors and strings for night and Light mode

--- app/src/main/java/com/route/islamic40gsunwed/fragments/TasbeehFragment.kt
+++ app/src/main/java/com/route/islamic40gsunwed/fragments/TasbeehFragment.kt
@@ -6,19 +6,25 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.route.islamic40gsunwed.R
+import com.route.islamic40gsunwed.databinding.FragmentTasbeehBinding
 
 class TasbeehFragment : Fragment() {
 
+    lateinit var binding: FragmentTasbeehBinding
+    var counter: Int = 0
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_tasbeeh, container, false)
+        binding = FragmentTasbeehBinding.inflate(inflater)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
+        binding.btnSubhanaAllah.setOnClickListener {
+            counter++
+            binding.tvNumberOfTassbeh.text = counter.toString()
+        }
     }
 }
