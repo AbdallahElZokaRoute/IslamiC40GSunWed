6b8db2af748edcf31be6be61a29761a8e371feaa
kareem <kareemessam878@gmail.com>
2024-08-02 11:53:58 +0300

    tasbeh fragment by kareem

--- app/src/main/java/com/route/islamic40gsunwed/fragments/TasbeehFragment.kt
+++ app/src/main/java/com/route/islamic40gsunwed/fragments/TasbeehFragment.kt
@@ -4,10 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
 import com.route.islamic40gsunwed.R
 
 class TasbeehFragment : Fragment() {
+
+    private var counter:Int =0
+    lateinit var counterTextView: TextView
+    lateinit var tasbehBtn:CardView
+    lateinit var resetBtn:CardView
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,6 +26,22 @@ class TasbeehFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        counterTextView = view.findViewById(R.id.tasbeh_counter_text)
+        tasbehBtn = view.findViewById(R.id.tasbeh_btn)
+        resetBtn = view.findViewById(R.id.tasbeh_reset)
+
+        counter = 0
+        counterTextView.text = counter.toString()
+
+        tasbehBtn.setOnClickListener{
+            counter++
+            counterTextView.text=counter.toString()
+        }
+
+        resetBtn.setOnClickListener{
+            counter = 0
+            counterTextView.text=counter.toString()
+        }
 
     }
 }
